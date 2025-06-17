7faf9486a38ea46a2c84108fba8ae0b788a34d1d
Björn Petersen <git@bjoernpetersen.net>
2025-06-17 19:30:47 +0200

    Initialize NATS client

--- src/receiver/api.py
+++ src/receiver/api.py
@@ -1,16 +1,42 @@
+import json
 import logging
+from contextlib import asynccontextmanager
+from typing import Any
 
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from nats.aio.client import Client
 
 from receiver.config import init_config
 
 _LOG = logging.getLogger(__name__)
 
 config = init_config()
+client = Client()
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await client.connect(
+        config.nats.url,
+        allow_reconnect=True,
+    )
+
+    yield
+
+    await client.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+async def publish(bot_name: str, update: dict[str, Any]) -> None:
+    jetstream = client.jetstream()
+    await jetstream.publish(
+        subject=f"{config.nats.subject_namespace}.{bot_name}",
+        stream=config.nats.stream_name,
+        payload=json.dumps(update).encode(),
+    )
 
 
 @app.get("/", response_class=RedirectResponse)
